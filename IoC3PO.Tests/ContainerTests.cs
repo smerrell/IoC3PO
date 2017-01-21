3af08582aeeccc55576c84c753e2c202416d23a4
Sam Merrell <merrell.sam@gmail.com>
2017-01-20 21:09:50 -0700

    Initial commit
    Super basic creation of a class, no constraints or anything yet.

---
+++ IoC3PO.Tests/ContainerTests.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace IoC3PO.Tests
+{
+    public class ContainerTests
+    {
+        public interface IProtocolDroid { }
+        public class Threepio : IProtocolDroid { }
+
+        [Fact]
+        public void registers_implementation()
+        {
+            var container = new Container();
+            container.Register<IProtocolDroid, Threepio>();
+
+            var threepio = container.Resolve<IProtocolDroid>();
+            threepio.ShouldNotBeNull();
+            threepio.ShouldBeOfType<Threepio>();
+        }
+    }
+}
---
+++ IoC3PO/Container.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IoC3PO
+{
+    public class Container
+    {
+        private Dictionary<Type, object> types = new Dictionary<Type, object>();
+        public void Register<TInterface, TImplementation>()
+        {
+            var type = Activator.CreateInstance<TImplementation>();
+            types.Add(typeof(TInterface), type);
+        }
+
+        public TInterface Resolve<TInterface>()
+        {
+            return (TInterface) types[typeof(TInterface)];
+        }
+    }
+
+    public enum LifeCycle
+    {
+        Transient,
+        Singleton
+    }
+}
