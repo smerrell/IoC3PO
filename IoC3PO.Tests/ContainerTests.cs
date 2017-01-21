4196f4007fef1e8bf9f4c4fd417375cdfb6fb706
Sam Merrell <merrell.sam@gmail.com>
2017-01-20 21:50:52 -0700

    Handle singleton objects
    Defaults to Transient. The code is super rough at the moment, but will
    clean up as I go.

--- IoC3PO.Tests/ContainerTests.cs
+++ IoC3PO.Tests/ContainerTests.cs
@@ -5,18 +5,42 @@ namespace IoC3PO.Tests
 {
     public class ContainerTests
     {
+        private Container _container;
+
         public interface IProtocolDroid { }
         public class Threepio : IProtocolDroid { }
+
+        public ContainerTests()
+        {
+            _container = new Container();
+        }
 
         [Fact]
         public void registers_implementation()
         {
-            var container = new Container();
-            container.Register<IProtocolDroid, Threepio>();
+            _container.Register<IProtocolDroid, Threepio>();
 
-            var threepio = container.Resolve<IProtocolDroid>();
+            var threepio = _container.Resolve<IProtocolDroid>();
             threepio.ShouldNotBeNull();
             threepio.ShouldBeOfType<Threepio>();
+        }
+
+        [Fact]
+        public void resolving_a_transient_object_returns_a_new_object_each_time()
+        {
+            _container.Register<IProtocolDroid, Threepio>();
+            var threepio1 = _container.Resolve<IProtocolDroid>();
+            var threepio2 = _container.Resolve<IProtocolDroid>();
+            threepio1.ShouldNotBeSameAs(threepio2);
+        }
+
+        [Fact]
+        public void registered_singleton_is_a_singleton()
+        {
+            _container.Register<IProtocolDroid, Threepio>(LifeCycle.Singleton);
+            var threepio1 = _container.Resolve<IProtocolDroid>();
+            var threepio2 = _container.Resolve<IProtocolDroid>();
+            threepio1.ShouldBeSameAs(threepio2);
         }
     }
 }
--- IoC3PO/Container.cs
+++ IoC3PO/Container.cs
@@ -1,20 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IoC3PO
 {
     public class Container
     {
-        private Dictionary<Type, object> types = new Dictionary<Type, object>();
+        private Dictionary<Type, TypeRegistration> types = new Dictionary<Type, TypeRegistration>();
         public void Register<TInterface, TImplementation>()
         {
-            var type = Activator.CreateInstance<TImplementation>();
-            types.Add(typeof(TInterface), type);
+            Register<TInterface, TImplementation>(LifeCycle.Transient);
+        }
+
+        public void Register<TInterface, TImplementation>(LifeCycle lifecycle)
+        {
+            types.Add(typeof(TInterface), new TypeRegistration(lifecycle, typeof(TImplementation)));
         }
 
         public TInterface Resolve<TInterface>()
         {
-            return (TInterface) types[typeof(TInterface)];
+            var typeRegistration = types[typeof(TInterface)];
+            if (typeRegistration.LifeCycle == LifeCycle.Singleton)
+            {
+                if (typeRegistration.RegisteredObject == null)
+                {
+                    typeRegistration.RegisteredObject = Activator.CreateInstance(typeRegistration.RegisteredType);
+                }
+
+                return (TInterface) typeRegistration.RegisteredObject;
+            }
+
+            return (TInterface) Activator.CreateInstance(typeRegistration.RegisteredType);
+        }
+    }
+
+    public class TypeRegistration
+    {
+        public LifeCycle LifeCycle { get; }
+        public Type RegisteredType { get; }
+        public object RegisteredObject { get; set; }
+
+        public TypeRegistration(LifeCycle lifeCycle, Type registeredType)
+        {
+            LifeCycle = lifeCycle;
+            RegisteredType = registeredType;
         }
     }
 
